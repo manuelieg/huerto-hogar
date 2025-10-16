4b06d602f09030a2b4a9bf288ff56f174d28726d
manuelieg <manue.espinozag@duocuc.cl>
2025-10-16 20:25:54 -0300

    se actualiza json productos y agrega categorias

---
+++ src/data/categorias.js
@@ -0,0 +1,26 @@
+const productos = [
+    {
+        id: "FR",
+        imagen: " ",
+        nombre: "Frutas Frescas",
+        link: " "
+    },
+    {
+        id: "VR",
+        imagen: " ",
+        nombre: "Verduras Orgánicas",
+        link: " "
+    },
+    {
+        id: "PO",
+        imagen: "",
+        nombre: "Productos Orgánicos",
+        link: " "
+    },
+    {
+        id: "PL",
+        imagen: "",
+        nombre: "Productos Lácteos",
+        link: " "
+    },
+];
--- src/data/productos.js
+++ src/data/productos.js
@@ -1,12 +1,82 @@
 const productos = [
     {
-        id: 1,
+        id: "FR001",
         imagen: " ",
         nombre: "Manzana Fuji",
-        categoria: "Fruta",
+        categoria: "Frutas Frescas",
         descripcion: "Manzanas Fuji crujientes y dulces, cultivadas en el Valle del Maule. Perfectas para meriendas saludables o como ingrediente en postres. Estas manzanas son conocidas por su textura firme y su sabor equilibrado entre dulce y ácido.",
         precio: 1200,
-        stock: 500,
+        stock: 150,
+        link: " "
+    },
+    {
+        id: "FR002",
+        imagen: " ",
+        nombre: "Naranjas Valencia",
+        categoria: "Frutas Frescas",
+        descripcion: "Jugosas y ricas en vitamina C, estas naranjas Valencia son ideales para zumos frescos y refrescantes. Cultivadas en condiciones climáticas óptimas que aseguran su dulzura y jugosidad.",
+        precio: 1000,
+        stock: 200,
+        link: " "
+    },
+    {
+        id: "FR003",
+        imagen: " ",
+        nombre: "Plátanos Cavendish ",
+        categoria: "Frutas Frescas",
+        descripcion: "Plátanos maduros y dulces, perfectos para el desayuno o como snack energético. Estos plátanos son ricos en potasio y vitaminas, ideales para mantener una dieta equilibrada. ",
+        precio: 800,
+        stock: 250,
+        link: " "
+    },
+    {
+        id: "VR001",
+        imagen: " ",
+        nombre: "Zanahorias Orgánicas",
+        categoria: "Verduras Orgánicas",
+        descripcion: "Zanahorias crujientes cultivadas sin pesticidas en la Región de O'Higgins. Excelente fuente de vitamina A y fibra, ideales para ensaladas, jugos o como snack saludable.",
+        precio: 900,
+        stock: 100,
+        link: " "
+    },
+    {
+        id: "VR002",
+        imagen: " ",
+        nombre: "Espinacas Frescas",
+        categoria: "Verduras Orgánicas",
+        descripcion: "Espinacas frescas y nutritivas, perfectas para ensaladas y batidos verdes. Estas espinacas son cultivadas bajo prácticas orgánicas que garantizan su calidad y valor nutricional.",
+        precio: 700,
+        stock: 80,
+        link: " "
+    },
+    {
+        id: "VR003",
+        imagen: "",
+        nombre: "Espinacas Frescas",
+        categoria: "Pimientos Tricolores",
+        descripcion: "Pimientos rojos, amarillos y verdes, ideales para salteados y platos coloridos. Ricos en antioxidantes y vitaminas, estos pimientos añaden un toque vibrante y saludable a cualquier receta.",
+        precio: 1500,
+        stock: 120,
+        link: " "
+    },
+    {
+        id: "PO001",
+        imagen: "",
+        nombre: "Miel Orgánica",
+        categoria: "Productos Orgánicos",
+        descripcion: "Pimientos rojos, amarillos y verdes, ideales para salteados y platos coloridos. Ricos en antioxidantes y vitaminas, estos pimientos añaden un toque vibrante y saludable a cualquier receta.",
+        precio: 5000,
+        stock: 50,
+        link: " "
+    },
+    {
+        id: "PL001",
+        imagen: "",
+        nombre: "Leche Entera",
+        categoria: "Productos Lácteos",
+        descripcion: " ",
+        precio: 1000,
+        stock: 100,
         link: " "
     },
 ];
