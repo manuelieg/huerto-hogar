99b6fb50b29cc7de8ce3223dc435cab819deaef2
Mateo Sebastian Lara Osses <mate.lara@duocuc.cl>
2025-10-17 20:36:21 -0300

    Se actualiza el home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
-function Home(){
-    return(
-        <h1>Home</h1>
-    )
-}
+
+import React from 'react';
+import CarouselMain from '../components/Carousel.jsx';
+import { getCarouselItems } from "../data/carouseldata.js"; 
+
+
+const Home = () => {
+    const datosCarrusel = getCarouselItems();
+    console.log("DATOS CARGADOS Y FUNCIONALES:", datosCarrusel); 
+
+    return (
+        <div className="container mx-auto p-4">
+           <CarouselMain imagenes={datosCarrusel} />
+        </div>
+    );
+};
 
 export default Home;
