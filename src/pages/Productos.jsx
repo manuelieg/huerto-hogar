69a06d62042732fe1eb43ea67af2b691c9e3668f
Mateo Sebastian Lara Osses <mate.lara@duocuc.cl>
2025-10-18 01:02:28 -0300

    Se cambia el css y el jsx de productos

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,7 +1,18 @@
-function Productos(){
-    return(
-        <h1>Aqui van los Productos</h1>
-    )
-}
+import React from 'react';
+import ProductGrid from '../components/ProductGrid.jsx'; 
+
+const Productos = () => {
+    return (
+        <div className="container mt-4 mb-5">
+            <h1 className="fw-bolder mb-4 text-dark text-center">Catálogo de Productos Frescos</h1>
+            <p className="lead text-center text-muted mb-5">
+                Explora nuestra selección de verduras y frutas orgánicas, listas para llevar a tu hogar.
+            </p>
+            
+            <ProductGrid />
+            
+        </div>
+    );
+};
 
 export default Productos;
